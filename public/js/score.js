a700c534e276e9f9e0b49d5ca3c6eb6edaaa50fd
zeynobe <zeynepgspor@gmail.com>
2024-08-15 17:02:42 +0300

    scoreboard colors changed

---
+++ public/js/score.js
@@ -0,0 +1,10 @@
+/*
+
+     
+      
+
+     function writescoreboard() {
+        document.querySelector('#scoretable tbody').innerHTML= (' <tr  class="table-success"><th scope="row" class="text-center">1</th><td>POKEMON MASTER</td><td>3500</td></tr>');
+     }
+
+     writescoreboard();*/ 
--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -4,7 +4,7 @@ var db = require('../database')
 
 /* GET home page. */
 router.get('/scoreboard', (req, res, next) => {
-  const query = 'SELECT user_id, score FROM scoreboard';
+  const query = 'SELECT username, user_id, score FROM scoreboard JOIN users ON scoreboard.user_id = users.id WHERE 1 ORDER BY score DESC LIMIT 10 ' ;
   db.all(query, [], (err, rows) => {
     if (err) {
       console.error('Sorgu hatası:', err.message); // Hata mesajı
@@ -19,4 +19,13 @@ router.get('/scoreboard', (req, res, next) => {
 module.exports = router;
 
 
+/*var express = require('express');
+var router = express.Router();
 
+
+// GET home page. 
+router.get('/scoreboard', function(req, res, next) {
+  res.render('scoreboard');
+});
+
+module.exports = router; */
