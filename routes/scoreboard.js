ec063b590ff3aa55c01285edd447a77002f62d35
zeynobe <zeynepgspor@gmail.com>
2024-08-15 15:24:03 +0300

    database added in scoreboard

--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../database')
 
 /* GET home page. */
-router.get('/scoreboard', function(req, res, next) {
-  res.render('scoreboard');
+router.get('/scoreboard', (req, res, next) => {
+  const query = 'SELECT user_id, score FROM scoreboard';
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      console.error('Sorgu hatası:', err.message); // Hata mesajı
+      res.status(500).send('Sorgu hatası');
+    } else {
+          res.render('scoreboard', { rows: rows});
+          console.log(rows);
+    }
+  }); 
 });
 
 module.exports = router;
+
+
+
